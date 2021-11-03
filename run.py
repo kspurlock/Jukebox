c3e326a17e38dc152f31067dc7dac22bbc4ed15b
kspurlock <kdspurlock@moreheadstate.edu>
2021-11-03 03:49:23 -0400

    Added python Spotify prototype, added environment (env) for testing

--- run.py
+++ run.py
@@ -1,9 +1,7 @@
 from jukebox import app
 
-# To Kian and Rece #
-# To run this use:
-#   - source env/Scripts/activate in linux console
-#   - .\env\Scripts\activate in windows console
+# To run this:
+# - pip install -r requirements.txt in console
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ spotify_sub_backend/spotify_creds.py
@@ -0,0 +1,3 @@
+client_id = "d5bf099821e44c9ebf883855e178c731"
+client_secret = "8ac58375e2474fb096fe663d727e9ee2"
+redirect_URI = "https://google.com/"
