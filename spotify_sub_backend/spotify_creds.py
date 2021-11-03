758dd748c5a1103a6ec4f0cda23664b620321bdb
kspurlock <kdspurlock@moreheadstate.edu>
2021-11-03 04:12:27 -0400

    Minor edit to main.html and spotify_creds

--- spotify_sub_backend/spotify_creds.py
+++ spotify_sub_backend/spotify_creds.py
@@ -1,3 +1,3 @@
-client_id = "d5bf099821e44c9ebf883855e178c731"
-client_secret = "8ac58375e2474fb096fe663d727e9ee2"
-redirect_URI = "https://google.com/"
+client_id = "d5bf099821e44c9ebf883855e178c731" #SPOTIPY_CLIENT_ID
+client_secret = "8ac58375e2474fb096fe663d727e9ee2" #SPOTIPY_CLIENT_SECRET
+redirect_URI = "https://google.com/" #SPOTIPY_REDIRECT_URI
