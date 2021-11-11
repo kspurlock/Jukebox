c1b298c4cb89d33a9bcdb06d0ea092b8fa80960e
kianworld <kianworld@live.com>
2021-11-10 21:36:12 -0500

    Updated run.py instructions / requirements.txt

--- run.py
+++ run.py
@@ -1,9 +1,8 @@
 from jukebox import app
 
 # To run this:
-# - pip install -r requirements.txt in console
-# - .\env\Scripts\activate.bat
-# - run without debugging
+# - "python -m pip install -r requirements.txt" in console
+# - "python run.py" in console
 # - click on https://127.0.0.1:5000/ in terminal
 
 if __name__ == "__main__":
