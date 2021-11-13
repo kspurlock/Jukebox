a74ecd8593cfb6b90db3ede25ac09c70e47cc8f2
kspurlock <kdspurlock@moreheadstate.edu>
2021-11-12 21:00:17 -0500

    Delete spotify_creds.py

--- tests_and_prototypes/spotify_api_backend/spotify_creds.py
+++
@@ -1,3 +0,0 @@
-client_id = "d5bf099821e44c9ebf883855e178c731" #SPOTIPY_CLIENT_ID
-client_secret = "8ac58375e2474fb096fe663d727e9ee2" #SPOTIPY_CLIENT_SECRET
-redirect_URI = "https://google.com/" #SPOTIPY_REDIRECT_URI
